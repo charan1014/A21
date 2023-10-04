390484f9822cbcb3c8f15c0b3e9ef79bd58cfc8f
charan1014 <21eg106a32@anurag.edu.in>
2023-10-04 17:40:11 +0530

    charan1014

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT
-
 var gracefulShutdown;
-var dbURI  = "mongodb://0.0.0.0:27017/";
+var dbURI  = "mongodb://0.0.0.0:27017/Shiva";
 if (process.env.NODE_ENV === 'Production') {
-    dbURI = process.env.MONGODB_URI;
+     dbURI = process.env.MONGODB_URI;
 }
 mongoose.connect(dbURI); 
 mongoose.connection.on('connected', function () {
      console.log('Mongoose connected to  ${dbURI}'); 
 });
 mongoose.connection.on('error', function (err) {
- console.log('Mongoose connection error: ' + err); 
+     console.log('Mongoose connection error: ' + err); 
 }); 
 mongoose.connection.on('disconnected', function()  {
      console.log('Mongoose disconnected'); 
 }); 
-require('./locations');
+
+require('./locations')
+
